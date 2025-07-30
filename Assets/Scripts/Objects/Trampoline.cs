6b14b696346ce2f35b11dd0dad3d3d11eac09e66
JottaFreitass <rm90599@estudante.fieb.edu.br>
2025-07-30 15:08:37 -0300

    trampoline / dunas - fase 2

---
+++ Assets/Scripts/Objects/Trampoline.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trampoline : MonoBehaviour
+{
+    [SerializeField]
+    float Impulso;
+    [SerializeField]
+    Animator animator;
+
+
+    void OnCollisionEnter2D(Collision2D colisao)
+    {
+        if(colisao.gameObject.CompareTag("Player"))
+        {
+            colisao.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * Impulso, ForceMode2D.Impulse);
+            animator.SetBool("Trampoline_Jump", true);
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D colisao)
+    {
+        if(colisao.gameObject.CompareTag("Player"))
+        {
+            animator.SetBool("Trampoline_Jump", false);
+        }
+    }
+}
