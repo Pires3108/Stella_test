97890051d379bf70b3d4e37808f385705597a363
João Vitor de Freitas Baptista <rm90599@estudante.fieb.edu.br>
2025-08-01 10:02:53 -0300

    correções cenário fase 1 e 2

--- Assets/Scripts/Criaturas/StateMachineDescriptor.cs
+++ Assets/Scripts/Criaturas/StateMachineDescriptor.cs
@@ -1,5 +1,27 @@
+using System;
+using Unity.VisualScripting; // Certifique-se de que este using está presente, se necessário
+
 namespace Unity.VisualScripting
 {
+    // Definição mínima do atributo Descriptor
+    [AttributeUsage(AttributeTargets.Class)]
+    public class DescriptorAttribute : Attribute
+    {
+        public DescriptorAttribute(Type type) { }
+    }
+
+    // Definição mínima de StateMachine
+    public class StateMachine { }
+
+    // Definição mínima de MachineDescription
+    public class MachineDescription { }
+
+    // Definição mínima de MachineDescriptor
+    public class MachineDescriptor<T1, T2>
+    {
+        public MachineDescriptor(T1 target) { }
+    }
+
     [Descriptor(typeof(StateMachine))]
     public sealed class StateMachineDescriptor : MachineDescriptor<StateMachine, MachineDescription>
     {
